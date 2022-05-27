511ae36afd709858ae698695a440f65a8074d1d4
SHOU6439 <shian35777@gmail.com>
2022-05-27 13:57:22 +0900

    station6を挑戦中

--- src/App.js
+++ src/App.js
@@ -2,15 +2,38 @@
 
 import * as React from 'react'
 import './App.css'
+import { useState } from 'react'
 
 /**
- * 
+ *
  * @type {React.FC}
  */
 export const App = () => {
+  const [dogUrl, setDogUrl] = useState([]);
+  const updateImage = () => {
+    fetch("https://dog.ceo/api/breeds/image/random")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          setDogUrl(() => { return result.message });
+        },
+        (error) => {
+          console.log(error)
+        }
+      );
+  }
+
   return (
     <div>
-      <h2>Hello, world!</h2>
+      <header>
+        <h1>Dog App</h1>
+      </header>
+      <p>犬の画像を表示するサイトです</p>
+      <img src={dogUrl}></img>
+      <div>
+        <button onClick={updateImage}>
+          更新
+        </button>
+      </div>
     </div>
-  )
-}
+  )};
